0de3c551429b3146033534f49cceeecfc75eda49
Winterfel17 <59329490+Winterfel17@users.noreply.github.com>
2020-07-06 23:04:05 -0500

    Se agrego plantilla para index
    se agrego una nueva plantilla para el index de la pagina

--- resources/views/admin/admin.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <admin users="{{ $users }}"></admin>
-   {{-- <example-component></example-component> --}}
-    {{-- <div id="app">
-        <admin></admin>
-    </div> --}}
-@endsection
